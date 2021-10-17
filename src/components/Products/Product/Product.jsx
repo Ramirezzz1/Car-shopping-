8bc292441ceb765d7f2fbca3d48c2cf39b568ab2
Ramirezz1 <ronaldramirezzz1@gmail.com>
2021-10-17 17:11:00 -0400

    images missing

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
+
+import useStyles from './styles';
+import Products from '../Products';
+
+const Product = ({ product }) => {
+    const classes = useStyles();
+
+
+
+    return (
+        <Card className={classes.root}>
+            <CardMedia className={classes.media} image={product.media} title={product.name} />
+            <CardContent>
+                <div className={classes.cardContent}>
+                    <Typography variant="h5" gutterbottom>
+                        {product.name}
+                    </Typography>
+                    <Typography variant="h5">
+                        {product.price.formatted_with_symbol}
+                    </Typography>
+                </div>
+                <Typography dangerouslySetInnerHTML={{__html:product.description}} variant="body2" color="textSecondary"/>
+            </CardContent>
+            <CardActions disableSpacing className={classes.cardActions}>
+                <IconButton arial-label="Add to Cart">
+                    <AddShoppingCart />
+                </IconButton>
+            </CardActions>
+        </Card>
+    )
+}
+
+export default Product;
