10c95afcac16eb512b5777433b06d7b7eb68ca98
Ramirezz1 <ronaldramirezzz1@gmail.com>
2021-10-17 22:07:36 -0400

    increment buttons bugs

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
 import React, { useState, useEffect } from "react";
 import { commerce } from "./lib/commerce";
 import { Products, Navbar, Cart } from './components';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
   const [products, setProducts] = useState([]);
   const [cart,setCart] = useState({});
   
-
+//api call for products
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
 
     setProducts(data);
   }
-
+/// api call for cart items
   const fetchCart = async () =>{
     setCart(await commerce.cart.retrieve())
   }
-  
+  //add to cart
   const handleAddToCart = async (productId, quanitity) => {
-    const item = await commerce.cart.add(productId, quanitity);
+    const { cart } = await commerce.cart.add(productId, quanitity);
 
-    setCart(item.cart);
+    setCart(cart);
   }
+//buttons functionality
+  const handleUpdateCartQty = async (productId, quanitity) => {
+    const { cart } = await commerce.cart.update(productId, {quanitity});
+
+    setCart(cart);
+  }
+/// remove from cart 
+  const handleRemoveFromCart = async (productId) => {
+    const { cart } = await commerce.cart.remove(productId); 
+
+    setCart(cart);
+  }
+  /// empty cart
+  const handleEmptyCart = async () => {
+    const { cart } = await commerce.cart.empty();
+
+    setCart(cart);
+  }
+
   useEffect(() => {
     fetchProducts();
     fetchCart();
@@ -33,13 +53,26 @@ const App = () => {
   // console.log(products)
 
   return (
-      <div>
-        <Navbar totalItems={cart.total_items} />
-        {/* <Products products={products} onAddToCart={handleAddToCart} /> */}
-        <Cart cart={cart} />
+      <Router>
+        <div>
+          <Navbar totalItems={cart.total_items} />
+          <Switch>
+            <Route exact path="/">
+              <Products products={products} onAddToCart={handleAddToCart} />
+            </Route>
+            <Route exact path="/cart">
+              <Cart 
+              cart={cart} 
+              handleUpdateCartQty = {handleAddToCart}
+              handleRemoveFromCart = {handleRemoveFromCart}
+              handleEmptyCart = {handleEmptyCart}
+              />
+            </Route>
+          </Switch>
 
-      </div>
-  )
+        </div>
+      </Router>
+  );
 }
 
 export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,34 @@
 import React from 'react';
 import {AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
+import { Link, useLocation } from 'react-router-dom'
 
 import logo from '../../assests/logo.PNG';
 import useStyles from './styles';
 
 const Navbar = ({totalItems}) => {
     const classes = useStyles();
+    const location = useLocation();
+
     
     return (
         <>
             <AppBar position="fixed" className={classes.appBar} color="inherit">
                 <Toolbar>
-                    <Typography variant="h6" className={classes.title} color="inherit">
+                    <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
                         <img src={logo} alt="Jersey Imports" height="25px" className={classes.image} />
                         Jersey Imports
                     </Typography>
                     <div className={classes.grow} />
+                    {location.pathname == '/' && (
                     <div className={classes.button}>
-                        <IconButton aria-label="Show cart items" color="inherit">
+                        <Link to="/cart">View your cart</Link>
+                        <IconButton component={Link} aria-label="Show cart items" color="inherit">
                             <Badge badgeContent={totalItems} color="secondary">
                                 <ShoppingCart />
                             </Badge>
                         </IconButton>
-                    </div>
+                    </div> )}
                 </Toolbar> 
             </AppBar>
         </>
